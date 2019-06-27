ab2c5410bcaaebec5daeedbc1201302270b1660e
Joseph Salisbury <salisbury.joseph@gmail.com>
2019-06-27 10:20:05 +0100

    Adds report command towards OpsGenie reporting

---
+++ cmd/report/command.go
@@ -0,0 +1,72 @@
+package report
+
+import (
+	"io"
+	"os"
+
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"github.com/giantswarm/oncall-scheduler/cmd/report/alert"
+	"github.com/spf13/cobra"
+)
+
+const (
+	name        = "report"
+	description = "Report on information"
+)
+
+type Config struct {
+	Logger micrologger.Logger
+	Stderr io.Writer
+	Stdout io.Writer
+}
+
+func New(config Config) (*cobra.Command, error) {
+	if config.Logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
+	}
+	if config.Stderr == nil {
+		config.Stderr = os.Stderr
+	}
+	if config.Stdout == nil {
+		config.Stdout = os.Stdout
+	}
+
+	var err error
+
+	var alertCmd *cobra.Command
+	{
+		c := alert.Config{
+			Logger: config.Logger,
+			Stderr: config.Stderr,
+			Stdout: config.Stdout,
+		}
+
+		alertCmd, err = alert.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	f := &flag{}
+
+	r := &runner{
+		flag:   f,
+		logger: config.Logger,
+		stderr: config.Stderr,
+		stdout: config.Stdout,
+	}
+
+	c := &cobra.Command{
+		Use:   name,
+		Short: description,
+		Long:  description,
+		RunE:  r.Run,
+	}
+
+	f.Init(c)
+
+	c.AddCommand(alertCmd)
+
+	return c, nil
+}
