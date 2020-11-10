b69c18809f0e4b743d720ede4308dffab93499ce
vol <me@volodymyrstoiko.com>
2020-11-10 14:02:15 +0200

    Remove copypaste

--- pkg/opsgenieclient/client.go
+++ pkg/opsgenieclient/client.go
@@ -26,10 +26,6 @@ func New(config Config) (*Client, error) {
 		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
 	}
 
-	if config.Logger == nil {
-		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
-	}
-
 	if config.APIKey == "" {
 		return nil, microerror.Maskf(invalidConfigError, "%T.APIKey must not be empty", config)
 	}
